a4b0da93c864902ce678d6c03276e2be968bb157
PhapNguyen <vietphappro1@gmail.com>
2024-02-28 18:01:13 +0700

    Update View login signup

---
+++ Group01_QuanLyLuanVan/Group01_QuanLyLuanVan/Model/Const.cs
@@ -0,0 +1,14 @@
+﻿using Group01_QuanLyLuanVan.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Group01_QuanLyLuanVan.Model
+{
+    public class Const : BaseViewModel
+    {
+        public static string _localLink = System.Reflection.Assembly.GetExecutingAssembly().Location.Remove(System.Reflection.Assembly.GetExecutingAssembly().Location.IndexOf(@"bin\Debug"));
+    }
+}
