af7a1ac4314e8ec4c1027310a60ef7b204bee684
PhapNguyen <vietphappro1@gmail.com>
2024-03-05 17:27:41 +0700

    add teacher main view

---
+++ Group01_QuanLyLuanVan/Group01_QuanLyLuanVan/ViewModel/TeacherMainViewModel.cs
@@ -0,0 +1,52 @@
+﻿using Group01_QuanLyLuanVan.Model;
+using Group01_QuanLyLuanVan.View;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows;
+
+namespace Group01_QuanLyLuanVan.ViewModel
+{
+    public class TeacherMainViewModel : BaseViewModel
+    {
+        public ICommand Loadwd { get; set; }
+        public static Frame MainFrame { get; set; }
+
+        public ICommand LoadPageCM { get; set; }
+
+        public ICommand SignoutCM { get; set; }
+        public ICommand HomeCM { get; set; }
+
+        public TeacherMainViewModel()
+        {
+
+            SignoutCM = new RelayCommand<FrameworkElement>((p) => { return p == null ? false : true; }, (p) =>
+            {
+                FrameworkElement window = GetParentWindow(p);
+                var w = window as Window;
+                if (w != null)
+                {
+                    w.Hide();
+                    LoginView w1 = new LoginView();
+                    w1.ShowDialog();
+                    w.Close();
+                }
+            });
+
+            FrameworkElement GetParentWindow(FrameworkElement p)
+            {
+                FrameworkElement parent = p;
+
+                while (parent.Parent != null)
+                {
+                    parent = parent.Parent as FrameworkElement;
+                }
+                return parent;
+            }
+        }
+    }
+}
