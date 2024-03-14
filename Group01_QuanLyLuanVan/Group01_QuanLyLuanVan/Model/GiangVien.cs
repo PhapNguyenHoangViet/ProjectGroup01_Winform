162d63bff5f7177324e8a17cebfd1415a390905e
thanhhuyendc <102838052+thanhhuyendc@users.noreply.github.com>
2024-03-14 13:37:51 +0700

    model

--- Group01_QuanLyLuanVan/Group01_QuanLyLuanVan/Model/GiangVien.cs
+++ Group01_QuanLyLuanVan/Group01_QuanLyLuanVan/Model/GiangVien.cs
@@ -1,14 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Group01_QuanLyLuanVan.Model
 {
-    public class SinhVien
+    public class GiangVien
     {
-        private string sinhVienId;
+        private string giangVienId;
         private string hoTen;
         private DateTime ngaySinh;
         private string gioiTinh;
@@ -17,13 +18,12 @@ namespace Group01_QuanLyLuanVan.Model
         private string sdt;
         private string khoaId;
         private string username;
-        private string nhomId;
 
-        public SinhVien() { }
+        public GiangVien () { }
 
-        public SinhVien(string sinhVienId, string hoTen, DateTime ngaySinh, string gioiTinh, string diaChi, string email, string sdt, string khoaId, string username, string nhomId)
+        public GiangVien(string giangVienId, string hoTen, DateTime ngaySinh, string gioiTinh, string diaChi, string email, string sdt, string khoaId, string username)
         {
-            this.sinhVienId = sinhVienId;
+            this.giangVienId = giangVienId;
             this.hoTen = hoTen;
             this.ngaySinh = ngaySinh;
             this.gioiTinh = gioiTinh;
@@ -32,18 +32,16 @@ namespace Group01_QuanLyLuanVan.Model
             this.sdt = sdt;
             this.khoaId = khoaId;
             this.username = username;
-            this.nhomId = nhomId;
         }
 
-        public string SinhVienId { get => sinhVienId; set => sinhVienId = value; }
+        public string GiangVienId { get => giangVienId; set => giangVienId = value; }
         public string HoTen { get => hoTen; set => hoTen = value; }
-        public DateTime NgaySinh { get => ngaySinh; set => ngaySinh = value; }
+        public DateTime NgaySinh { get => ngaySinh; set => ngaySinh = value;}
         public string GioiTinh { get => gioiTinh; set => gioiTinh = value; }
         public string DiaChi { get => diaChi; set => diaChi = value; }
         public string Email { get => email; set => email = value; }
         public string SDT { get => sdt; set => sdt = value; }
         public string KhoaId { get => khoaId; set => khoaId = value; }
         public string Username { get => username; set => username = value; }
-        public string NhomId { get => nhomId; set => nhomId = value; }
     }
 }
---
+++ Group01_QuanLyLuanVan/Group01_QuanLyLuanVan/Model/TaiKhoan.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Group01_QuanLyLuanVan.Model
+{
+    public class TaiKhoan
+    {
+        private string username;
+        private string password;
+        private string mail;
+        private string quyen;
+        private string code;
+        private int trangThai;
+        
+        public TaiKhoan() { }
+
+        public TaiKhoan(string username, string password, string mail, string quyen, string code, int trangThai)
+        {
+            this.username = username;
+            this.password = password;
+            this.mail = mail;
+            this.quyen = quyen;
+            this.code = code;
+            this.trangThai = trangThai;
+        }
+
+        public string Username { get => username; set => username = value; }
+        public string Password { get => password; set => password = value; }
+        public string Mail { get => mail; set => mail = value; }
+        public string Quyen { get => quyen; set => quyen = value; }
+        public string Code { get => code; set => code = value; }
+        public int  TrangThai { get => trangThai; set => trangThai = value; }
+    }
+}
